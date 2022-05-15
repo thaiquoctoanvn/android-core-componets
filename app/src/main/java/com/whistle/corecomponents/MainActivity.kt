d27a70e32b09308b24823bf5f37c66077bf64982
Toan Thai <toan.thai@gumiviet.com>
2022-05-15 16:58:47 +0700

    feat: build project

---
+++ app/src/main/java/com/whistle/corecomponents/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.whistle.corecomponents
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.whistle.corecomponents.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        val navGraphIds =
+            listOf(R.navigation.nav_intro, R.navigation.nav_category, R.navigation.nav_profile)
+
+        binding?.bnv?.setUpWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.fcv,
+            intent = intent
+        )
+
+        binding?.bnv?.setOnItemSelectedListener {
+            setMainTitle(it.title.toString())
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    fun setMainTitle(title: String) {
+        binding?.tvMainTitle?.text = title
+    }
+}
